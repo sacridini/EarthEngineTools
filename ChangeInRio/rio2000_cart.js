3a5c14e9b33b5b113502787066a511ad78df4c60
sacridini <eduardolacerdageo@gmail.com>
2017-07-29 02:19:45 -0300

    comments!

--- ChangeInRio/rio2000_cart.js
+++ ChangeInRio/rio2000_cart.js
@@ -1,24 +1,40 @@
+// Delimita a região de interesse (roi) e 
+// a data min e max para coleta das imagens
 var roi = ee.Geometry.Point ([-43.25,-22.90])
 var start = ee.Date('2000-01-01');
 var finish = ee.Date('2000-12-31');
 
+// Melhor imagem do ano 2000 (com menor quantidade de nuvem)
 var image = ee.Image(l5
     .filterBounds(roi)
     .filterDate(start, finish)
     .sort('CLOUD_COVER')
     .first());
+
+// Reseta o mapa nessas coordenadas.
+// Esta etapa é importante para garantir que as imagens finais sejam exportadas
+// com o mesmo tamanho
 Map.setCenter(-43.25,-22.90, 10);  
+
 // Map.addLayer(image, {bands: ['B4', 'B3', 'B2'], max: 0.3}, 'image');
 
+// Gera os índices (NDVI, NDBI e NDWI)
 var rio2000_ndvi = image.normalizedDifference(['B4','B3']).rename('NDVI');
 var rio2000_ndbi = image.normalizedDifference(['B5','B4']).rename('NDBI');
 var rio2000_ndwi = image.normalizedDifference(['B2','B4']).rename('NDWI');
 var rio2000_idx = image.addBands([rio2000_ndvi, rio2000_ndbi, rio2000_ndwi]);
 
+// Junta todas as amostras coletadas em uma única variável
 var newfc = urbano.merge(verde).merge(agua).merge(pasto);
+
+// Seleciona as bandas para serem utilizadas pelo classificador
+// Primeiro uma sem os índices
 var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
+
+// E outra com eles... Vamos usar esta! Viva os índices! :) 
 var bands_idx = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7','NDVI','NDBI','NDWI'];
 
+// Gera o treinador a partir das amostras coletadas
 var training = rio2000_idx.select(bands_idx).sampleRegions({
   collection: newfc, 
   properties: ['landcover'], 
@@ -26,20 +42,23 @@ var training = rio2000_idx.select(bands_idx).sampleRegions({
 });
 // print(training)
 
+// Inicializa (cria) o classificador
 var classifier = ee.Classifier.cart().train({
   features: training, 
   classProperty: 'landcover', 
   inputProperties: bands_idx
 });
 
-
+// Imprime no console a árvore criada
 print(classifier.explain());
+
+// Gera a classificação
 var classified = rio2000_idx.select(bands_idx).classify(classifier);
 
-
+// Mostra a classificação na tela principal
 Map.addLayer(classified, {min: 0, max: 3, palette: ['#0099ff','red', '#003300', '#00cc00']});
 
-// Export the image, specifying scale and region.
+// Exporta o resultado final para o Google Drive
 Export.image.toDrive({
   image: classified,
   description: 'rio2000_cart_idx',
--- ChangeInRio/rio2010_cart.js
+++ ChangeInRio/rio2010_cart.js
@@ -1,25 +1,40 @@
+// Delimita a região de interesse (roi) e 
+// a data min e max para coleta das imagens
 var roi = ee.Geometry.Point ([-43.25,-22.90])
 var start = ee.Date('2010-01-01');
 var finish = ee.Date('2010-12-31');
 
+// Melhor imagem do ano 2000 (com menor quantidade de nuvem)
 var image = ee.Image(l5
     .filterBounds(roi)
     .filterDate(start, finish)
     .sort('CLOUD_COVER')
     .first());
 
+// Reseta o mapa nessas coordenadas.
+// Esta etapa é importante para garantir que as imagens finais sejam exportadas
+// com o mesmo tamanho
 Map.setCenter(-43.25,-22.90, 10);  
+
 // Map.addLayer(image, {bands: ['B4', 'B3', 'B2'], max: 0.3}, 'image');
 
+// Gera os índices (NDVI, NDBI e NDWI)
 var rio2010_ndvi = image.normalizedDifference(['B4','B3']).rename('NDVI');
 var rio2010_ndbi = image.normalizedDifference(['B5','B4']).rename('NDBI');
 var rio2010_ndwi = image.normalizedDifference(['B2','B4']).rename('NDWI');
 var rio2010_idx = image.addBands([rio2010_ndvi, rio2010_ndbi, rio2010_ndwi]);
 
+// Junta todas as amostras coletadas em uma única variável
 var newfc = urbano.merge(verde).merge(agua).merge(pasto);
+
+// Seleciona as bandas para serem utilizadas pelo classificador
+// Primeiro uma sem os índices
 var bands = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'];
+
+// E outra com eles... Vamos usar esta! Viva os índices! :) 
 var bands_idx = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7','NDVI','NDBI','NDWI'];
 
+// Gera o treinador a partir das amostras coletadas
 var training = rio2010_idx.select(bands_idx).sampleRegions({
   collection: newfc, 
   properties: ['landcover'], 
@@ -27,20 +42,23 @@ var training = rio2010_idx.select(bands_idx).sampleRegions({
 });
 // print(training)
 
+// Inicializa (cria) o classificador
 var classifier = ee.Classifier.cart().train({
   features: training, 
   classProperty: 'landcover', 
   inputProperties: bands_idx
 });
 
-
+// Imprime no console a árvore criada
 print(classifier.explain());
+
+// Gera a classificação
 var classified = rio2010_idx.select(bands_idx).classify(classifier);
 
-
+// Mostra a classificação na tela principal// Mostra a classificação na tela principal
 Map.addLayer(classified, {min: 0, max: 3, palette: ['red', '#0099ff', '#003300', '#00cc00']});
 
-// Export the image, specifying scale and region.
+// Exporta o resultado final para o Google Drive
 Export.image.toDrive({
   image: classified,
   description: 'rio2010_cart_idx',
